a6d0a63d25fa5e0b26259cb858bc20c6aea260ef
Aric Repp <aricmrepp@gmail.com>
2020-04-28 22:07:20 -0400

    Finished Part 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+// import NASACard from "./NASACard"
 import "./App.css";
 
 function App() {
+
+  const [Apod, setApod] = useState([]);
+  const [Date, setDate] = useState('2012-03-14');
+
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=aYxd3GuYOUk6bPiApkAZE0oACNbiwaxqaOi9GOzD&date=${Date}`)
+    .then(response => {
+        // console.log(response.data)
+        setApod(response.data);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun!! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div className="card">
+        <div className="search_container">
+          <input className="search" placeholder="Search Date YYYY-MM-DD..."></input>
+        </div>
+        <div className="img_container">
+          <img className="cardImg" src={Apod.hdurl} alt="picture"/>
+        </div>
+        <div className="text_container">
+          <h1 className="title">{Apod.title}</h1>
+          <p className="explanation">Date: {Apod.date}</p>
+          <p className="explanation">{Apod.explanation}</p>
+         <p className="explanation">{Apod.copyright}</p>
+        </div>
+      </div>
+      
     </div>
+
+    
   );
 }
 
